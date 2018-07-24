4c4da91a1697016244c7a2496dbe21f3692fe91b
admin <admin@10.137.138.150>
2018-07-24 15:55:49 +0900

    Chapter05

---
+++ src/icehs/science/chapter05/SumAndAverageTest.java
@@ -0,0 +1,20 @@
+package icehs.science.chapter05;
+
+public class SumAndAverageTest {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		double avg = 0;
+		
+		for(int i = 1 ; i <= 500 ; i++) {
+			sum += i;
+		}
+		//평균구하는 식
+		avg = (double)sum / 500;
+		
+		System.out.println("합계 : " + sum);
+		System.out.println("평균 : " + avg);
+
+	}
+
+}
