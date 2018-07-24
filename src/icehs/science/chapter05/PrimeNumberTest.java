af2a97dde9513602f37271be0acf61dc4ec5b919
admin <admin@10.137.138.150>
2018-07-24 16:25:31 +0900

    chapter05

---
+++ src/icehs/science/chapter05/PrimeNumberTest.java
@@ -0,0 +1,16 @@
+package icehs.science.chapter05;
+
+public class PrimeNumberTest {
+
+	public static void main(String[] args) {
+		int num1 = 23;
+		int num2 = 26;
+		/*
+		 * teams.microsoft.com
+		 * id@naver.com
+		 * 
+		 */
+
+	}
+
+}
