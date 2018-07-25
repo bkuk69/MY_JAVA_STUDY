02f442fe13a845b104d389f9658ec96a582c5089
admin <admin@10.137.138.150>
2018-07-25 17:45:12 +0900

    Chapter07

---
+++ src/icehs/science/chapter06/StudentTest.java
@@ -0,0 +1,27 @@
+package icehs.science.chapter06;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student hong = new Student();
+		hong.name = "홍길동";
+		hong.stuID = "S001";
+		hong.korean = 94;
+		hong.english = 80;
+		hong.math = 89;
+		
+		hong.printStudentInfo();
+		hong.calculateAVG();
+		
+		System.out.println();
+		System.out.println("* 학번을 변경합니다.");
+		System.out.println();
+		
+		hong.changeStudentID("STU0001");
+		hong.printStudentInfo();
+		
+		
+		
+	}
+
+}
